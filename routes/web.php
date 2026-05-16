f5fb4ca01f0b499654a103946e7158c5e1e5f805
Mostafa Nouh <mustafa.nouh.nouh@gmail.com>
2026-05-16 18:43:19 +0300

    Add remote migration route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/run-migration', function () {
+    try {
+        $status = Artisan::call('migrate --force');
+        
+        return response()->json([
+            'status' => 'success',
+            'output' => Artisan::output()
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => $e->getMessage()
+        ], 500);
+    }
 });
