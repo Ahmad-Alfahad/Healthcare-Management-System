b6bec094da6d37662d3742055b9b5fe79fdec154
Mostafa Nouh <mustafa.nouh.nouh@gmail.com>
2026-05-16 14:26:43 +0300

    add .clevercloud for init clever host

--- azure-startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-available/default
-service nginx restart
-php /home/site/wwwroot/artisan config:cache
-php /home/site/wwwroot/artisan route:cache
