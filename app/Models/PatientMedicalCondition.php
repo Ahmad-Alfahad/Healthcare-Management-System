d0166061ff230864e9a4ff0dea79297701306939
Ahmad-Alfahad <ahmad222alfahad@gmail.com>
2026-05-18 16:45:13 +0300

    create medical condition model

---
+++ app/Models/PatientMedicalCondition.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PatientMedicalCondition extends Model
+{
+    //
+    use HasFactory;
+
+    protected $fillable = [
+
+        'patient_id',
+
+        'medical_condition_id',
+
+        'diagnosed_at',
+
+        'notes',
+    ];
+
+    protected $casts = [
+
+        'diagnosed_at' => 'date',
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(
+            Patient::class
+        );
+    }
+
+    public function medicalCondition()
+    {
+        return $this->belongsTo(
+            MedicalCondition::class
+        );
+    }
+}
